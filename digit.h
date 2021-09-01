22bd7ae1c663dfa2d9f6125b57e6cb10b3ce5a3f
xSnowHeadx <13079267+xSnowHeadx@users.noreply.github.com>
2021-09-01 15:18:28 +0200

    made defines in digit.h more clear

--- digit.h
+++ digit.h
@@ -9,12 +9,12 @@
 #define DIGIT_H_
 
 // settings
-#define EIGHT_DIGIT false     	// true if your machine has 8 digits
-#define ORIGIN_SENSOR false   	// true if you installed origin sensor
-#define ORIGIN_COMPENSATION 50 	// compensation of origin mark position
-#define ORIGIN_THRES 3500      	// photo reflector sensor threshold
-#define PRE_MOVE	1			// set to 1 to start move earlier to reach the end position at correct time
-#define DEBUG true
+#define EIGHT_DIGIT 		false     	// true if your machine has 8 digits
+#define ORIGIN_SENSOR 		false   	// true if you installed origin sensor
+#define ORIGIN_COMPENSATION 50 			// compensation of origin mark position
+#define ORIGIN_THRES 		3500      	// photo reflector sensor threshold
+#define PRE_MOVE 			true		// true to start move earlier to reach the end position just in time
+#define DEBUG 				true		// true for additional serial debug messages
 
 #if EIGHT_DIGIT
 #define DIGIT 8
