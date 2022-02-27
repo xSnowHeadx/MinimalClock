df117592aa8d0ebb82215bb31bcecd89fe2521d3
xSnowHeadx <13079267+xSnowHeadx@users.noreply.github.com>
2022-02-27 13:44:04 +0100

    undo last addition

--- MinimalClock.ino
+++ MinimalClock.ino
@@ -315,7 +315,6 @@ void loop()
 	static int lastmin = -1;
 	struct tm *tmtime;
 
-	bool test = wtaClient.GetDST();
 	askFrequency = 60 * 60 * 1000;
 	while (((netEpoch = wtaClient.GetCurrentTime()) == locEpoch) || (!netEpoch))
 	{
